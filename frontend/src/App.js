1cfae3710054f31c12d6ae63299df341304b7233
Sharonraju64 <sharonraju1243@gmail.com>
2024-05-07 15:33:06 +0530

    Handle Logout Issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import ProtectedRoute from './pages/protectedRoute';
+import PageLoading from './pages/pageLoading';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/>
+          <Route path='/page-loading' element={<PageLoading/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/protectedRoute.js
+++ frontend/src/pages/protectedRoute.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../firebase';
 import { Navigate } from 'react-router-dom';
+import PageLoading from './pageLoading';
 
 const ProtectedRoute = ({children}) => {
     const [user, isLoading] = useAuthState(auth);
 
     if(isLoading){
-        console.log('loading...');
+        return <PageLoading />
     }
     if(!user){
         return <Navigate to='/login' />
