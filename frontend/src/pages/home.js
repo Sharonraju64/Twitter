bdd7bf6ddd34b3f13af4586561a3a4e430dc207d
Sharonraju64 <sharonraju1243@gmail.com>
2024-05-07 17:32:24 +0530

    Style the Sidebar

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { signOut } from 'firebase/auth';
+import auth from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import Sidebar from './sidebar';
 import Feed from './feed';
 import Widgets from './widgets';
 
 const Home = () => {
+    const user = useAuthState(auth);
+    
+    const handleLogout = () =>{
+        signOut(auth)
+    }
     return (
         <div className='app'>
-            <Sidebar />
+            <Sidebar handleLogout={handleLogout} user={user}/>
             <Feed />
             <Widgets />
         </div>
--- frontend/src/pages/sidebar.js
+++ frontend/src/pages/sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/sidebar.css';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SidebarOptions from './sidebarOptions';
@@ -12,12 +12,26 @@ import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import MoreIcon from '@mui/icons-material/More'
 import DoneIcon from '@mui/icons-material/Done';
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem';
+import { Avatar, Button, Divider, IconButton, ListItemIcon } from '@mui/material';
 
 
-const Sidebar = () => {
+const Sidebar = ({handleLogout, user}) => {
+
+    const [anchorE1, setAnchorE1] = useState(null);
+    const openMenu = Boolean(anchorE1);
+
+    const handleClick = e =>{
+        setAnchorE1(e.currentTarget);
+    }
+    const handleClose = () =>{
+        setAnchorE1(null);
+    }
+
     return (
         <div className='sidebar'>
-            <TwitterIcon className='sidebar_twittericon' />
+            <TwitterIcon className='sidebar_twitterIcon' />
             <SidebarOptions active Icon={HomeIcon} text='Home' />
             <SidebarOptions active Icon={SearchIcon} text='Explore' />
             <SidebarOptions active Icon={NotificationsIcon} text='Notifications' />
@@ -26,6 +40,43 @@ const Sidebar = () => {
             <SidebarOptions active Icon={ListAltIcon} text='Lists' />
             <SidebarOptions active Icon={PermIdentityIcon} text='Profile' />
             <SidebarOptions active Icon={MoreIcon} text='More' />
+            
+            <Button variant='outlined' className='sidebar_tweet'>
+                Tweet
+            </Button>
+
+            <div className='Profile_info'>
+                <Avatar src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' />
+                <div className='user_info'>
+                    <h4>Sharon Raju</h4>
+                    <h5>@sharonraju64</h5>
+                </div>
+                <IconButton 
+                    size='small'
+                    sx={{ml: 2}}
+                    aria-controls={openMenu ? "basic-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={openMenu ? "true" : undefined}
+                    onClick={handleClick}
+                >
+                    <MoreHorizIcon />
+                </IconButton>
+                <Menu id='basic-menu' anchorEl={anchorE1} open={openMenu} onClick={handleClose} onClose={handleClose}>
+                    <MenuItem className='Profile_info1'>
+                        <Avatar src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' />
+                        <div className='user_info subUser_info'>
+                            <div>
+                                <h4>Sharon Raju</h4>
+                                <h5>@sharonraju64</h5>
+                            </div>
+                            <ListItemIcon className='done_icon'><DoneIcon /></ListItemIcon>
+                        </div>
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleClose}>Add an existing account</MenuItem>
+                    <MenuItem onClick={handleLogout}>Log out @sharonraju64</MenuItem>
+                </Menu>
+            </div>
         </div>
     );
 };
