12786bf7e336ac11c70c644f6d1228d400cfa26d
Sharonraju64 <sharonraju1243@gmail.com>
2024-06-11 02:15:24 +0530

    Make the UserPost Visible

--- frontend/src/pages/feed.js
+++ frontend/src/pages/feed.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/feed.css';
 import TweetBox from './tweetBox';
+import axios from 'axios';
+import Post from '../pages/post';
 const Feed = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/api/post/getpost')
+        .then(res=> res.json())
+        .then(data =>{
+            setPosts(data);
+        })        
+    },[])
+
     return (
         <div className='feed'>
             <div className='feed__header'>
                 <h2>Home</h2>
             </div>
             <TweetBox />
+            {
+                posts.map(p=><Post key={p._id} p={p} />)
+            }
         </div>
     );
 };
