96c333ccc522eddc30baed20344030b97d158f20
Sharonraju64 <sharonraju1243@gmail.com>
2024-06-11 23:46:40 +0530

    Design The Widgets Section

--- frontend/src/pages/widgets.js
+++ frontend/src/pages/widgets.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import {TwitterTweetEmbed, TwitterTimelineEmbed} from 'react-twitter-embed';
 import '../styles/widgets.css';
 
 const Widgets = () => {
     return (
-        <div>
-            <h1>Widgets</h1>        
+        <div className='widgets'>
+            <div className='widgets_input'>
+                <SearchIcon className='widgets_searchIcon' />
+                <input type='text' placeholder='SearchTwitter' />
+            </div>
+            <div className='widgets_wedgetContainer'>
+                <h2>What's happening</h2>   
+            </div>
+
+            <TwitterTweetEmbed 
+                tweetId={'1557187138352861186'}
+            />
+            <TwitterTimelineEmbed 
+                sourceType='profile'
+                screenName='elonmusk'
+                options={{height: 400}}
+            />
         </div>
     );
 };
