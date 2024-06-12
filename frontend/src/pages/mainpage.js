86a6c7d43c5db79960e8080d6cd351af44404e2e
Sharonraju64 <sharonraju1243@gmail.com>
2024-06-12 16:36:49 +0530

    Design the Profile Section Part 3

--- frontend/src/pages/mainpage.js
+++ frontend/src/pages/mainpage.js
@@ -3,14 +3,17 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {useNavigate} from 'react-router-dom';
 import CenterFocusWeakIcon from '@mui/icons-material/CenterFocusWeak';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
+import LockResetIcon from '@mui/icons-material/LockReset';
 import AddLinkIcon from '@mui/icons-material/AddLink';
 import Post from '../pages/post';
+import axios from 'axios';
 import '../styles/mainpage.css';
 import useLoggedinUser from '../hooks/useloggedinuser';
 
 const MainPage = ({user}) => {
     const navigate = useNavigate();
     const [loggedInUser] = useLoggedinUser();
+    const [isLoading, setIsLoading] = useState('');
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
@@ -21,8 +24,38 @@ const MainPage = ({user}) => {
         })
     }, [posts])
     const username = user?.email?.split('@')[0];
-    const handleUploadCoverImage = ()=>{
-        console.log("object");
+
+    const handleUploadCoverImage = (e) =>{
+        setIsLoading(true);
+        const image = e.target.files[0];
+        // console.log(image);
+        const formData = new FormData();
+        formData.set('image',image)
+
+        axios.post("https://api.imgbb.com/1/upload?key=00264694231da06a2a520041a073cac1", formData)
+        .then(res => {
+            const url = res.data.data.display_url;
+            setIsLoading(false);
+            if (url) {
+                console.log(url);
+            }
+        })
+    }
+    const handleUploadProfileImage = (e) =>{
+        setIsLoading(true);
+        const image = e.target.files[0];
+        // console.log(image);
+        const formData = new FormData();
+        formData.set('image',image)
+
+        axios.post("https://api.imgbb.com/1/upload?key=00264694231da06a2a520041a073cac1", formData)
+        .then(res => {
+            const url = res.data.data.display_url;
+            setIsLoading(false);
+            if (url) {
+                console.log(url);
+            }
+        })
     }
 
     return (
@@ -34,10 +67,15 @@ const MainPage = ({user}) => {
                     {
                         <div>
                             <div className='coverImageContainer'>
-                                <img src={loggedInUser[0]?.coverImage?loggedInUser[0]?.coverimage: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'} alt='' className='coverImage' />
+                                <img src={loggedInUser[0]?.coverImage?loggedInUser[0]?.coverimage: 'https://www.proactivechannel.com/Files/BrandImages/Default.jpg'} alt='' className='coverImage' />
                                 <div className='hoverCoverImage'>
                                     <label htmlFor='image' className='imageIcon'>
-                                        <CenterFocusWeakIcon className='photoIcon' />
+                                        {
+                                            isLoading?
+                                            <LockResetIcon className='photoIcon photoIconDisabled'/>
+                                            :
+                                            <CenterFocusWeakIcon className='photoIcon' />
+                                        }
                                     </label>
                                     <div className='imageIcon_tweetButton'>
                                         <input type='file' id='image' className='imageInput' onChange={handleUploadCoverImage} />
@@ -47,18 +85,23 @@ const MainPage = ({user}) => {
                             <div className='avatar-img'>
                                 <div className='avatarContainer'>
                                 <img src={loggedInUser[0]?.coverImage?loggedInUser[0]?.coverimage: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'} alt='' className='avatar' />
-                                </div>
-                                <div className='hoverAvaterImage'>
+                                <div className='hoverAvatarImage'>
                                     <div className='imageIcon_tweetButton'>
-                                    <label htmlFor='profileImage' className='imageIcon'>
-                                        <CenterFocusWeakIcon className='photoIcon' />
-                                    </label>
-                                    <div className='imageIcon_tweetButton'>
-                                        <input type='file' id='profileImage' className='imageInput' onChange={handleUploadCoverImage} />
+                                        <label htmlFor='profileImage' className='imageIcon'>
+                                        {
+                                            isLoading?
+                                            <LockResetIcon className='photoIcon photoIconDisabled'/>
+                                            :
+                                            <CenterFocusWeakIcon className='photoIcon' />
+                                        }
+                                        </label>
+                                        <div className='imageIcon_tweetButton'>
+                                            <input type='file' id='profileImage' className='imageInput' onChange={handleUploadProfileImage} />
+                                        </div>
                                     </div>
                                     </div>
                                 </div>
-                                <div className='userinfo'>
+                                <div className='userInfo'>
                                     <div>
                                         <h3 className='heading-3'>
                                             {loggedInUser[0]?.name ? loggedInUser[0]?.name : user && user?.displayName}
