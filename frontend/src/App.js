51bb6a51b4fde8bb02c39565073cb5666103180b
Sharonraju64 <sharonraju1243@gmail.com>
2024-06-28 16:08:06 +0530

    home file changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
+import Home from './pages/Feed/Home';
 import Login from './pages/Login/Login';
 import Signup from './pages/Login/Signup';
 import ProtectedRoute from './pages/protectedRoute';
--- frontend/src/pages/Feed/Home.js
+++ frontend/src/pages/Feed/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useUserAuth } from "../Firebase/UserAuthContext";
+import { useUserAuth } from "../../Firebase/UserAuthContext";
 import { useNavigate } from "react-router";
-import Sidebar from './Sidebar/Sidebar';
+import Sidebar from '../Sidebar/Sidebar';
 import { Outlet } from 'react-router-dom';
-import Widgets from './Widgets/Widgets';
+import Widgets from '../Widgets/Widgets';
 
 const Home = () => {
     const { logOut, user } = useUserAuth();
